73d2fd5afd4ea163a2930846fc67b9053a454fcc
Fab <fab.mckay@gmail.com>
2019-06-06 20:05:52 +0200

    :wrench: Ajout de config à TopicPoster

--- Client/src/main/java/com/polyauto/jboss/TopicPoster.java
+++ Client/src/main/java/com/polyauto/jboss/TopicPoster.java
@@ -1,26 +1,44 @@
 package com.polyauto.jboss;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 import javax.jms.*;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import java.io.Serializable;
 
+@Service
 public class TopicPoster
 {
     private static TopicConnectionFactory tcf;
     private static TopicConnection conn;
     private static Topic topic;
 
+    @Autowired
+    private Environment env0;
+
+    private static Environment env;
+
+    @PostConstruct
+    private void initAuthenticator()
+    {
+        env = env0;
+    }
+
     public static void init() throws JMSException, NamingException {
 
         try {
             InitialContext iniCtx = new InitialContext();
-            Object tmp = iniCtx.lookup("java:/ConnectionFactory");
+
+            Object tmp = iniCtx.lookup(env.getProperty("polyauto.jboss.connectionFactory"));
 
             tcf = (TopicConnectionFactory) tmp;
-            conn = tcf.createTopicConnection("jmsuser", "jmsepul98!");
-            topic = (Topic) iniCtx.lookup("java:jboss/exported/topic/PolyAutoTopic");
+            conn = tcf.createTopicConnection(env.getProperty("polyauto.jboss.jmsuser"), env.getProperty("polyauto.jboss.jmspassword"));
+            topic = (Topic) iniCtx.lookup(env.getProperty("polyauto.jboss.topic"));
             // Send the specified number of messages
         } catch (JMSException e) {
             throw e;
